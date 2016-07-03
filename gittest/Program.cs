2f37fd031463f0b0d4522b6a6a18322e0440e888
lepiklark <aleksandartomic88@gmail.com>
2016-07-03 22:11:11 +0200

    This is one change in child branch

--- gittest/Program.cs
+++ gittest/Program.cs
@@ -11,6 +11,8 @@ namespace gittest
         static void Main(string[] args)
         {
             Console.WriteLine("This is the first master commit.");
+
+            Console.WriteLine("I am now in my new branch, I am going to check in this");
         }
     }
 }
